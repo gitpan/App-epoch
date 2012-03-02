048c31a5ff47d1d647192fc19a11d0537a286e3c
Traian Nedelea <tron@cpan.org>
2012-03-02 19:20:15 +0000

    Import of TRON/App-epoch-1.001 from CPAN.
    gitpan-cpan-distribution: App-epoch
    gitpan-cpan-version:      1.001
    gitpan-cpan-path:         TRON/App-epoch-1.001.tar.gz
    gitpan-cpan-author:       TRON
    gitpan-cpan-maturity:     released

--- lib/App/epoch.pm
+++ lib/App/epoch.pm
@@ -2,8 +2,39 @@ use strict;
 use warnings;
 package App::epoch;
 
-our $VERSION = 1;
+our $VERSION = 1.001;
 # ABSTRACT: Converts fuzzy time into local time
+
+=head1 NAME
+
+App::epoch - Convert between fuzzy times in other timezones
+
+=head1 SYNOPSYS
+
+    $ epoch tomorrow 2pm GMT
+    2012-03-03 08:00:00 CST
+
+    $ epoch tomorrow 2pm GMT -- hh:mm:ss
+    08:00:00
+
+=head1 DESCRIPTION
+
+C<App::epoch> installs a command called, oddly enough, C<epoch> that converts
+fuzzy time strings (see L<Time::ParseDate>) into local times, formatted by
+L<Time::Format>.
+
+=head1 COMMAND LINE USAGE
+
+    $ epoch --help
+    epoch: Fuzzy time conversion to localtime
+
+    Usage:
+        epoch time [--] [format]
+
+        `time` can be any string that Time::ParseDate accepts
+        `format` can be any string that Time::Format::time_format accepts
+
+=cut
 
 use Time::ParseDate;
 use Time::Format qw(time_format);
